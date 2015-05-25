f4c93fb3a44026023df30872767166d6bd85199e
Drew Proud <drew.proud@hotmail.com>
2015-05-25 20:58:46 +0000

    New package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'drewproud:decision-tree',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'A package for handling complex decision tree logic',
   // URL to the Git repository containing the source code for this package.
